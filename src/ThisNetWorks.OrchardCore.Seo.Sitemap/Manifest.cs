e9b56c5aa9aa9df130c5e2994dfc8057911f6d9e
Dean Marcussen <dean.marcussen@gmail.com>
2019-01-27 12:43:51 +0000

    initial sitemap module in and showing in the admin

---
+++ src/ThisNetWorks.OrchardCore.Seo.Sitemap/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "ThisNetworks.OrchardCore.Seo.Sitemap",
+    Author = "ThisNetWorks",
+    Website = "https://github.com/ThisNetWorks/ThisNetWorks.OrchardCore.Seo",
+    Version = "0.0.1",
+    Description = "ThisNetworks Seo Sitemap Module provices dynamic sitemap generation services for OrchardCore",
+    Category = "Content"
+)]
