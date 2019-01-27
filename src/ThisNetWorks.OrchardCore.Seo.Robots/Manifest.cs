67ed8b5289ad481914d777c88dcb6ea9060cb175
Dean Marcussen <dean.marcussen@gmail.com>
2019-01-27 11:30:53 +0000

    initial robots project created and showing in admin

---
+++ src/ThisNetWorks.OrchardCore.Seo.Robots/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "ThisNetworks.OrchardCore.Seo.Robots",
+    Author = "ThisNetWorks",
+    Website = "https://github.com/ThisNetWorks/ThisNetWorks.OrchardCore.Seo",
+    Version = "0.0.1",
+    Description = "ThisNetworks Seo Robots Module provices robots.txt services for OrchardCore",
+    Category = "Content"
+)]
---
+++ src/ThisNetWorks.OrchardCore.Seo.Robots/Startup.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
+
+namespace ThisNetWorks.OrchardCore.Seo.Robots
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+        }
+    }
+}
