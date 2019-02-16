e2b15286e9269a6edd9bf17c1ba911c568da0029
Dean Marcussen <dean.marcussen@gmail.com>
2019-02-16 14:47:31 +0000

    updated metapart display to support liquid templtes in pagetitle and metadescription generation

--- src/ThisNetWorks.OrchardCore.Seo.Meta/Drivers/SeoMetaPartDisplay.cs
+++ src/ThisNetWorks.OrchardCore.Seo.Meta/Drivers/SeoMetaPartDisplay.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Fluid;
 using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Display.ContentDisplay;
 using OrchardCore.DisplayManagement.ModelBinding;
 using OrchardCore.DisplayManagement.Views;
+using OrchardCore.Liquid;
 using ThisNetWorks.OrchardCore.Seo.Meta.Models;
 using ThisNetWorks.OrchardCore.Seo.Meta.ViewModels;
 
@@ -16,50 +19,68 @@ namespace ThisNetWorks.OrchardCore.Seo.Meta.Drivers
     {
         private readonly IContentManager _contentManager;
         private readonly IServiceProvider _serviceProvider;
+        private readonly ILiquidTemplateManager _liquidTemplatemanager;
 
         public SeoMetaPartDisplay(
             IContentManager contentManager,
-            IServiceProvider serviceProvider
+            IServiceProvider serviceProvider,
+            ILiquidTemplateManager liquidTemplateManager
             )
         {
             _contentManager = contentManager;
             _serviceProvider = serviceProvider;
+            _liquidTemplatemanager = liquidTemplateManager;
         }
 
         public override IDisplayResult Display(SeoMetaPart part)
         {
-            return Initialize<SeoMetaPartViewModel>("SeoMetaPart", model => {
-                model.PageTitle = part.PageTitle;
-                model.MetaDescription = part.MetaDescription;
-                model.MetaKeywords = part.MetaKeywords;
-                model.SeoMetaPart = part;
-            }).Location("Detail", "Content:5");
+            return Initialize<SeoMetaPartViewModel>("SeoMetaPart", m => BuildDisplayViewModelAsync(m, part))
+                .Location("Detail", "Content:5");
         }
+
+
 
         public override IDisplayResult Edit(SeoMetaPart sitemapPart)
         {
-            return Initialize<SeoMetaPartViewModel>("SeoMetaPart_Edit", m =>
-            {
-                BuildViewModel(m, sitemapPart);
-            });
+            return Initialize<SeoMetaPartViewModel>("SeoMetaPart_Edit", m => BuildEditViewModel(m, sitemapPart));
         }
         
         public override async Task<IDisplayResult> UpdateAsync(SeoMetaPart model, IUpdateModel updater)
         {
             await updater.TryUpdateModelAsync(model, Prefix, t => t.PageTitle, t => t.MetaDescription, t => t.MetaKeywords);
-            
-
             return Edit(model);
         }
 
+        private async Task BuildDisplayViewModelAsync(SeoMetaPartViewModel model, SeoMetaPart part)
+        {
+            var templateContext = new TemplateContext();
+            templateContext.SetValue("ContentItem", part.ContentItem);
+            templateContext.MemberAccessStrategy.Register<SeoMetaPartViewModel>();
 
-        private void BuildViewModel(SeoMetaPartViewModel model, SeoMetaPart part)
+            using (var writer = new StringWriter())
+            {
+                await _liquidTemplatemanager.RenderAsync(part.PageTitle, writer, NullEncoder.Default, templateContext);
+                model.PageTitle = writer.ToString();
+            }
+            using (var writer = new StringWriter())
+            {
+                await _liquidTemplatemanager.RenderAsync(part.MetaDescription, writer, NullEncoder.Default, templateContext);
+                model.MetaDescription = writer.ToString();
+            }
+            using (var writer = new StringWriter())
+            {
+                await _liquidTemplatemanager.RenderAsync(part.MetaKeywords, writer, NullEncoder.Default, templateContext);
+                model.MetaKeywords = writer.ToString();
+            }
+            model.SeoMetaPart = part;
+        }
+
+        private void BuildEditViewModel(SeoMetaPartViewModel model, SeoMetaPart part)
         {
             model.PageTitle = part.PageTitle;
             model.MetaDescription = part.MetaDescription;
             model.MetaKeywords = part.MetaKeywords;
             model.SeoMetaPart = part;
-            
         }
     }
 }
