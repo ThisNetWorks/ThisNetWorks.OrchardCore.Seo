b93f1c826e4d1a403bc57ec71a3ccca78ee2edb2
Dean Marcussen <dean@buzzinteractive.co.uk>
2019-05-27 11:21:44 +0100

    Revert "move all to root folder, looking to resolve refs errors"
    This reverts commit 1a31cec94f877c07adb2f05b053292ed868af878.

---
+++ src/ThisNetWorks.OrchardCore.Seo.Meta/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "ThisNetworks.OrchardCore.Seo.Meta",
+    Author = "ThisNetWorks",
+    Website = "https://github.com/ThisNetWorks/ThisNetWorks.OrchardCore.Seo",
+    Version = "0.0.1",
+    Description = "ThisNetworks Seo Meta Module provices Seo meta description services for OrchardCore",
+    Category = "Content"
+)]
